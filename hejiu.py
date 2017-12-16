0e0d7517bfe20f7539d7c03128a88268167defcc
DLM8207 <877703305@qq.com>
2017-12-16 08:43:25 +0800

    喝酒题目

---
+++ hejiu.py
@@ -0,0 +1,19 @@
+﻿pijiu = 2.0
+qian = 10.0
+i = 0
+gaizicount = 0
+pingzicount = 0
+while qian >= pijiu:
+    qian -= pijiu
+    i+=1
+    gaizicount+=1
+    pingzicount+=1
+    print '第%d瓶啤酒，剩余%f钱,盖子数量%d,瓶子数量%d' % (i, qian, gaizicount, pingzicount)
+    if gaizicount==4:
+        qian += pijiu
+        gaizicount = 0
+        print '换盖子，第%d瓶啤酒，剩余%f钱,盖子数量%d,瓶子数量%d' % (i, qian, gaizicount, pingzicount)
+    if pingzicount==2:
+        qian += pijiu
+        pingzicount = 0
+        print '换瓶子，第%d瓶啤酒，剩余%f钱,盖子数量%d,瓶子数量%d' % (i, qian,gaizicount,pingzicount)
